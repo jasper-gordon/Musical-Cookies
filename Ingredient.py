34d3d386756e6e1bd32516600b53284eba19e2bf
Jasper <jgordon3@bowdoin.edu>
2020-09-30 21:32:16 -0400

    Added Ingredient stuff

--- Ingredient.py
+++ Ingredient.py
@@ -1,6 +1,18 @@
 
 class Ingredient: 
 
-    def __init__(self):
+    def __init__(self), name, amount, unit):
+        self.name = name
+        self.amount = amount
+        self.unit = unit
 
-        pass
+    #Returns string representation of this Ingredient
+    def __str__(self):
+        output = str(self.amount) + self.unit + self.name
+        return output
+
+    def __repr__(self):
+        return "Ingredient('{0}', {1}, '{2}')".format(self.name, self.amount, self.unit)
+
+    def get_amount(self):
+        return self.amount
