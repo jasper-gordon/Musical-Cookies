7fb8670ebb6a0d9610bb92b32f5f4061f5a44c36
Jasper <jgordon3@bowdoin.edu>
2020-10-07 22:10:13 -0400

    Updated so it can kinda handle bad input

--- music_testing.py
+++ music_testing.py
@@ -47,20 +47,29 @@ def ingredient_matcher(lyrics):
 #Returns the group of lyrics as a list of strings
 def lyric_gatherer(song_limit, artist_name):
     lyric_list = []
-    songs = genius.search_artist(artist_name, max_songs = song_limit, sort = "popularity" ).songs
-    for song in songs:
-        unique_lyrics = []
-        unfiltered_lyrics = song.lyrics.split()
-        #Only taking unique words from the lyrics to avoid repeats
-        for line in unfiltered_lyrics:
-            if line not in unique_lyrics:
-                #Adjusts case to lower to make sure it is comparable to ingredient_list
-                unique_lyrics.append(line.lower())
-        #Adding all new unique lyrics to the full list of the artist's lyrics
-        lyric_list.extend(unique_lyrics)
-    return lyric_list
+    try:
+        songs = genius.search_artist(artist_name, max_songs = song_limit, sort = "popularity" ).songs
+    except:
+        print("This artist input is invalid")
+        exit()
+    else:
+        print("made it this far")
+        for song in songs:
+            unique_lyrics = []
+            unfiltered_lyrics = song.lyrics.split()
+            #Only taking unique words from the lyrics to avoid repeats
+            for line in unfiltered_lyrics:
+                if line not in unique_lyrics:
+                    #Adjusts case to lower to make sure it is comparable to ingredient_list
+                    unique_lyrics.append(line.lower())
+            #Adding all new unique lyrics to the full list of the artist's lyrics
+            lyric_list.extend(unique_lyrics)
+        return lyric_list
 
-    
-test_list = lyric_gatherer(7, "Jimmy Buffet")
-print("Here are the lyrics by Jimmy Buffet that are also known ingredients: ")
-print(*ingredient_matcher(test_list), sep = ", ")
+artist = "Bob Dylan"    
+test_list = lyric_gatherer(5, artist)
+
+print("Here are the lyrics by " + artist + " that are also known ingredients: ")
+if test_list is not None:
+    print(*ingredient_matcher(test_list), sep = ", ")
+print ("yay made it")
