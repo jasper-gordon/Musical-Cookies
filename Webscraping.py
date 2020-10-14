4f72285b771ce852e656b3483259744ef9cf5f41
Tenzin Choezin <tchoezin@bowdoin.edu>
2020-10-13 21:27:00 -0500

    finished Webscraping documentation

--- Webscraping.py
+++ Webscraping.py
@@ -23,19 +23,26 @@ def web_scraper(url):
         the value.
     """
 
-    # sets a known browser user agent
+    # create a Request object that sets a known browser user agent
     req = Request(url, headers={"User-Agent": "Chrome"})
+
+    # opens the url within req and turns it into a readable format
     source = urlopen(req).read().decode('utf-8')
+
+    # create a Beautiful Soup object
     soup = BeautifulSoup(source, 'html.parser')
     
     # list of url links to different recipes
     link_list = []
 
-    # loop through the html of the website
+    # loop through the html of the website and find all the div tags with the specified class
     for section in soup.findAll('div', {"class": "listicle-page__cta-button"}):
+        # loop through all the a tags in each section to find the links to specific recipes
         for link in section.findAll('a'):
+            # add the link to the link list
             link_list.append(link.get('href'))
 
+    # stores the recipe name as the key and a list of the recipe's ingredients as the value
     ingredient_dict = {}
     
     for i in range(10):
@@ -52,8 +59,13 @@ def ingredient_dict_constructor(link_list, i, ingredient_dict):
         This function builds the ingredient dictionary.
     """
     
+    # create a Request object that sets a known browser user agent
     req = Request(link_list[i], headers={"User-Agent": "Chrome"})
+
+    # opens the url within req and turns it into a readable format
     source = urlopen(req).read().decode('utf-8')
+
+    # create a Beautiful Soup object
     soup = BeautifulSoup(source, 'html.parser')
 
 
@@ -61,10 +73,12 @@ def ingredient_dict_constructor(link_list, i, ingredient_dict):
 
     recipe_name = str(soup.find('h1', {"class": "recipe-title"}).text)
 
+    # access the recipes
     for recipe in soup.find('ul', {"class": "recipe-ingredients__list recipe-ingredients__collection splitColumns"}):
 
         for ingredient in recipe:
 
+            # makes sure we get actual ingredients added to the ingredient list
             if ingredient != soup.find('b', {"class": "sIngredient"}):
                 ingredient_list.append(str(ingredient))
 
